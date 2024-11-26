c198c378f24907be530847ec075464005da2cb76
AmosOO7 <ayokunene@gmail.com>
2024-11-26 11:37:34 +0100

    finished addpostscreen and pushed date to DB

--- app/(home)/Screens/AddPostScreen.tsx
+++ app/(home)/Screens/AddPostScreen.tsx
@@ -1,39 +1,118 @@
 import {
   View,
+  Image,
   Text,
   TextInput,
   StyleSheet,
   TouchableWithoutFeedback,
   Keyboard,
   TouchableOpacity,
+  Alert,
+  ActivityIndicator,
 } from "react-native";
 import React, { useEffect, useState } from "react";
-import { getFirestore, getDocs, collection } from "firebase/firestore";
+import { getFirestore, getDocs, collection, addDoc } from "firebase/firestore";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { app } from "../../../firebaseConfig";
 import { Formik } from "formik";
 import { Picker } from "@react-native-picker/picker";
-import { GestureResponderEvent } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import * as ImagePicker from "expo-image-picker";
+import Toast from "react-native-toast-message";
+import { useUser } from "@clerk/clerk-expo";
 
 export default function AddPostScreen() {
-  const [text, setText] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [image, setImage] = useState<string | null>(null);
+  const { user } = useUser();
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images", "videos"],
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
 
   const db = getFirestore(app);
   const [categoryList, setCategoryList] = useState<any[]>([]);
-  //use to get category list
+
   const getCategoryList = async () => {
+    setCategoryList([]);
     const querySnapshot = await getDocs(collection(db, "Category"));
     querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log("Data:", doc.data());
       setCategoryList((categoryList) => [...categoryList, doc.data()]);
     });
   };
+
   useEffect(() => {
     getCategoryList();
   }, []);
 
+  const onSubmitMethod = async (value: any) => {
+    setLoading(true);
+    if (!image) {
+      console.log("No image selected!");
+      // Handle case where image is not selected
+      return;
+    }
+
+    try {
+      // Convert URI to blob file
+      const resp = await fetch(image);
+      if (!resp.ok) {
+        throw new Error("Image fetch failed.");
+      }
+      const blob = await resp.blob();
+
+      // Set the storage path (could be dynamic)
+      const storage = getStorage();
+      const storageRef = ref(
+        storage,
+        `uploads/${new Date().toISOString()}_${Math.random()
+          .toString(36)
+          .substr(2, 9)}`
+      );
+
+      // Upload the image
+      const uploadResult = await uploadBytes(storageRef, blob);
+      console.log("Uploaded a blob or file!");
+
+      // Get the download URL of the uploaded image
+      const downloadURL = await getDownloadURL(uploadResult.ref);
+      console.log("File available at", downloadURL);
+      value.image = downloadURL;
+      value.userName = user?.fullName;
+      value.userEmail = user?.primaryEmailAddress?.emailAddress;
+      value.userImage = user?.imageUrl;
+      // Now store the download URL in your Firestore or Realtime Database
+      // For example, assuming you're using Firestore:
+      const db = getFirestore();
+      // Handle success (e.g., inform the user, update the UI)
+      console.log("Post successfully created with image URL");
+      //Storing the values on the DB
+      const docRef = await addDoc(collection(db, "UserPost"), value);
+      if (docRef.id) {
+        setLoading(false);
+        Alert.alert("Successful!!", "Post Added succesful");
+      }
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      // Handle error (e.g., show a toast or alert to the user)
+      // You could show an error message to the user here
+    }
+  };
+
   return (
-    <View className="p-16 mt-6">
+    <SafeAreaView className="pl-10 pr-10">
+      <Text className="text-[7vw] text-center">Add new post</Text>
+      <Text className="text-[4vw] text-center text-gray-500 mb-8">
+        Create a post and start selling
+      </Text>
       <Formik
         initialValues={{
           title: "",
@@ -41,20 +120,93 @@ export default function AddPostScreen() {
           category: "",
           address: "",
           price: "",
-          image: "",
+          userName: "",
+          UserEmail: "",
+          userImage: "",
         }}
-        onSubmit={(value) => console.log(value)}
+        onSubmit={(value) => onSubmitMethod(value)}
+        validate={(values) => {
+          let errors = false;
+          // if (!values.title) {
+          //   errors = true;
+          //   Toast.show({
+          //     type: "error",
+          //     text1: "Validation Error",
+          //     text2: "Title is required!",
+          //   });
+          // }
+          // if (!values.desc) {
+          //   errors = true;
+          //   Toast.show({
+          //     type: "error",
+          //     text1: "Validation Error",
+          //     text2: "Description is required!",
+          //   });
+          // }
+          // if (!values.address) {
+          //   errors = true;
+          //   Toast.show({
+          //     type: "error",
+          //     text1: "Validation Error",
+          //     text2: "Address is required!",
+          //   });
+          // }
+          // if (!values.price) {
+          //   errors = true;
+          //   Toast.show({
+          //     type: "error",
+          //     text1: "Validation Error",
+          //     text2: "Price is required!",
+          //   });
+          // }
+          // if (!image) {
+          //   errors = true;
+          //   Toast.show({
+          //     type: "error",
+          //     text1: "Validation Error",
+          //     text2: "Image is required!",
+          //   });
+          // }
+          // if (errors) throw new Error("Validation errors!");
+        }}
       >
         {({ handleChange, handleBlur, handleSubmit, values }) => (
           <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
             <View>
               <TouchableOpacity
-                onPress={
-                  handleSubmit as unknown as (e: GestureResponderEvent) => void
-                }
-                className="p-4 w-[50%] bg-black rounded-full mb-[1vh] self-end"
+                onPress={handleSubmit as unknown as (e: any) => void}
+                style={{ backgroundColor: loading ? "#ccc" : "#000" }}
+                disabled={loading}
+                className="p-4 w-[50%] bg-black rounded-2xl mb-[1vh] self-end"
               >
-                <Text className="text-center text-white">Submit</Text>
+                {loading ? (
+                  <ActivityIndicator color={"#fff"} />
+                ) : (
+                  <Text className="text-center text-white">Submit</Text>
+                )}
+              </TouchableOpacity>
+              <TouchableOpacity onPress={pickImage}>
+                {image ? (
+                  <Image
+                    source={{ uri: image }}
+                    style={{
+                      height: 100,
+                      width: 100,
+                      borderRadius: 20,
+                      marginBottom: 10,
+                    }}
+                  />
+                ) : (
+                  <Image
+                    source={require("../../../assets/images/ProjectImages/placeholder.jpg")}
+                    style={{
+                      height: 100,
+                      width: 100,
+                      borderRadius: 20,
+                      marginBottom: 10,
+                    }}
+                  />
+                )}
               </TouchableOpacity>
               <TextInput
                 style={styles.input}
@@ -75,7 +227,7 @@ export default function AddPostScreen() {
 
               <TextInput
                 style={styles.input}
-                placeholder="address"
+                placeholder="Address"
                 placeholderTextColor="#242424"
                 value={values?.address}
                 onChangeText={handleChange("address")}
@@ -83,7 +235,7 @@ export default function AddPostScreen() {
 
               <TextInput
                 style={styles.input}
-                placeholder="price"
+                placeholder="Price"
                 placeholderTextColor="#242424"
                 value={values?.price}
                 keyboardType="number-pad"
@@ -92,28 +244,32 @@ export default function AddPostScreen() {
               <Text className="text-[4vw] text-center mt-5">
                 Select Category
               </Text>
-              <Picker
-                selectedValue={values?.category}
-                onValueChange={handleChange("category")}
-                className="mb-10"
-              >
-                {categoryList &&
-                  categoryList.map((item, index) => (
-                    <Picker.Item
-                      key={index}
-                      label={item.name}
-                      value={item.name}
-                      color="#000"
-                    />
-                  ))}
-              </Picker>
+              <View style={styles.category}>
+                <Picker
+                  selectedValue={values?.category}
+                  onValueChange={handleChange("category")}
+                  className="mb-10"
+                >
+                  {categoryList &&
+                    categoryList.map((item, index) => (
+                      <Picker.Item
+                        key={index}
+                        label={item.name}
+                        value={item.name}
+                        color="#000"
+                      />
+                    ))}
+                </Picker>
+              </View>
             </View>
           </TouchableWithoutFeedback>
         )}
       </Formik>
-    </View>
+      <Toast />
+    </SafeAreaView>
   );
 }
+
 const styles = StyleSheet.create({
   input: {
     borderWidth: 1,
@@ -124,5 +280,9 @@ const styles = StyleSheet.create({
   },
   height: {
     height: 100,
+  },
+  category: {
+    borderWidth: 1,
+    borderRadius: 15,
   },
 });
--- app/(home)/Screens/HomeSceen.tsx
+++ app/(home)/Screens/HomeSceen.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
 
 export default function HomeSceen() {
   return (
     <View>
-      <Text>HomeSceen</Text>
+      <SafeAreaView>
+        <Text>HomeSceen</Text>
+      </SafeAreaView>
     </View>
   );
 }
